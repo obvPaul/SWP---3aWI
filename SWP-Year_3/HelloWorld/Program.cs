df364ec800355a980a1df78c881547eede2b8b2e
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2024-09-27 07:16:38 +0200

    Aufgabe 1 (Realisiert ich habe nie commitet)

--- SWP-Year_3/HelloWorld/Program.cs
+++ SWP-Year_3/HelloWorld/Program.cs
@@ -1,5 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 
+// Programmieraufgabe 1
+bool isInput = true;
 
-Console.Write("Hello, Onur!");
+Console.WriteLine("Schreibe etwas rein!");
+
+while (isInput)
+{
+    string input = Console.ReadLine();
+        if (int.TryParse(input, out int output))
+        {
+            int input2 = int.Parse(input);
+            Console.WriteLine("Deine Zahl wurde um 1 vergrößert: ");
+            Console.Write(input2 + 1);
+        }
+
+
+    if (input == "Beenden")
+    {
+        isInput = false;
+    }
+
+}
+
