18e2514497aff63bc1843b8ea00176906d6e63ca
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2024-09-27 10:15:19 +0200

    CleanCode änderungen

--- SWP-Year_3/HelloWorld/Program.cs
+++ SWP-Year_3/HelloWorld/Program.cs
@@ -1,28 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 
-// Programmieraufgabe 1
 bool isInput = true;
 
-Console.WriteLine("Schreibe etwas rein!");
+Console.WriteLine("Dieses Programm können Sie beenden, indem sie Beenden eingeben.");
 
-while (isInput)
-{
+while (isInput) {
     string input = Console.ReadLine();
-        if (int.TryParse(input, out int output))
-        {
+        if (int.TryParse(input, out int output)) {
             int input2 = int.Parse(input);
             Console.WriteLine("Deine Zahl wurde um 1 vergrößert: ");
             Console.Write(input2 + 1);
         }
-
-
-    if (input == "Beenden")
-    {
+    if (input == "Beenden") {
         isInput = false;
     }
-
 }
 
