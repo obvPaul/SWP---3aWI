1a526931826046b22264315da16ba1960aaa10f2
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2024-10-04 13:02:55 +0200

    Aufgabe 4 | FERTIG

--- SWP-Year_3/HelloWorld/Program.cs
+++ SWP-Year_3/HelloWorld/Program.cs
@@ -1,20 +1,35 @@
-﻿using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 
-bool isInput = true;
+Console.WriteLine("Geben Sie eine Zahl ein");
+string wertA = Console.ReadLine();
 
-Console.WriteLine("Dieses Programm können Sie beenden, indem sie Beenden eingeben.");
+Console.WriteLine("Geben Sie eine 1 ein, wenn Sie quadrieren wollen, eine 2, wenn Sie eine Wurzel ziehen wollen und eine 3, wenn sie fakultieren möchten");
+string eingabe = Console.ReadLine();
+double check = Convert.ToDouble(eingabe);
 
-while (isInput) {
-    string input = Console.ReadLine();
-        if (int.TryParse(input, out int output)) {
-            int input2 = int.Parse(input);
-            Console.WriteLine("Deine Zahl wurde um 1 vergrößert: ");
-            Console.Write(input2 + 1);
-        }
-    if (input == "Beenden") {
-        isInput = false;
-    }
+if (check == 1 ||check == 2 ||check == 3) {
+
+} 
+else {
+    Console.WriteLine("Die Zahl ist nicht Valid");
 }
 
+if (check == 1) {
+    Console.WriteLine("Geben Sie die zweite Zahl ein");
+    string wertB = Console.ReadLine();
+    Console.WriteLine("Ihr Ergebnis lautet: " + Math.Pow(Convert.ToDouble(wertA), Convert.ToDouble(wertB)));
+}
+else if (check == 2) {
+    Console.WriteLine("Ihr Ergebnis lautet: " + Math.Sqrt(Convert.ToDouble(wertA)));
+}
+else if (check ==3) {
+    double fakultaet = 1;
+        for (int i = 1; i <= Convert.ToDouble(wertA); i++)
+        {
+        fakultaet *= i;
+        
+        }
+    Console.WriteLine("Ihr Ergebnis lautet: " + fakultaet);
+}
