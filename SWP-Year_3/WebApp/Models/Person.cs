6ff0c799d5a98635d85c6a1a2ec2643d7fc1b89b
Melone <paul.oppeneiger@student.htldornbirn.at>
2025-03-21 13:09:07 +0100

    School-with-API | FINISHED

---
+++ SWP-Year_3/WebApp/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_02_Schule
+{
+    public class Person 
+    {
+        public string Name { get; set; }
+        public string Geschlecht { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+    }
+}
